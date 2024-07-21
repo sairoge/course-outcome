8ea0c4e7a984e68901b68879b83e1d107d5b60a3
sairoge <84916913+sairoge@users.noreply.github.com>
2024-07-21 18:09:52 +0530

    Initial Commit

---
+++ base/urls.py
@@ -0,0 +1,9 @@
+
+from django.urls import path,include
+from .views import authView,home
+
+urlpatterns = [
+    path("", home, name = "home"),
+    path("accounts/",include("django.contrib.auth.urls")),
+    path("signup/",authView, name="authView")
+]
